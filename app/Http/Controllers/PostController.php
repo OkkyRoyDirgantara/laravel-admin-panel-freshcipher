67e45ebfe72c1bd2f551d266a312b6dee47566b6
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-11-07 00:53:55 +0700

    Initialize project to git

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /// membuat validasi untuk title dan content wajib diisi
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        /// insert setiap request dari form ke dalam database via model
+        /// jika menggunakan metode ini, maka nama field dan nama form harus sama
+        Post::create($request->all());
+
+        /// redirect jika sukses menyimpan data
+        return redirect()->route('posts.index')
+            ->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        /// dengan menggunakan resource, kita bisa memanfaatkan model sebagai parameter
+        /// berdasarkan id yang dipilih
+        /// href="{{ route('posts.show',$post->id) }}
+        return view('posts.show',compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit',compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        /// membuat validasi untuk title dan content wajib diisi
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        /// mengubah data berdasarkan request dan parameter yang dikirimkan
+        $post->update($request->all());
+
+        /// setelah berhasil mengubah data
+        return redirect()->route('posts.index')
+            ->with('success','Post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        /// melakukan hapus data berdasarkan parameter yang dikirimkan
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success','Post deleted successfully');
+    }
+}
